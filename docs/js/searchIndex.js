383ea7d5f65007b21d4cb5089b6a1be9f57af228
jesus <jesus.gonzalez.garcia.2020@gmail.com>
2026-02-06 10:13:35 +0000

    Documentación técnica generada con PHPDocumentor

---
+++ docs/js/searchIndex.js
@@ -0,0 +1,39 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\Conexion",
+            "name": "Conexion",
+            "summary": "Clase\u0020de\u0020conexi\u00F3n\u0020a\u0020base\u0020de\u0020datos\u0020o\u0020LDAP",
+            "url": "classes/Conexion.html"
+        },                {
+            "fqsen": "\\Conexion\u003A\u003Aconectar\u0028\u0029",
+            "name": "conectar",
+            "summary": "",
+            "url": "classes/Conexion.html#method_conectar"
+        },                {
+            "fqsen": "\\Conexion\u003A\u003A\u0024host",
+            "name": "host",
+            "summary": "",
+            "url": "classes/Conexion.html#property_host"
+        },                {
+            "fqsen": "\\Conexion\u003A\u003A\u0024usuario",
+            "name": "usuario",
+            "summary": "",
+            "url": "classes/Conexion.html#property_usuario"
+        },                {
+            "fqsen": "\\Conexion\u003A\u003A\u0024password",
+            "name": "password",
+            "summary": "",
+            "url": "classes/Conexion.html#property_password"
+        },                {
+            "fqsen": "\\Conexion\u003A\u003A\u0024dbname",
+            "name": "dbname",
+            "summary": "",
+            "url": "classes/Conexion.html#property_dbname"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
